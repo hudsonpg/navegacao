dbc47b1c81b4abc57b35517c478a4d1635555f59
Hudson Pereira Gomes <hudson.pg@gmail.com>
2021-03-09 13:15:10 -0300

    adicionando Ícones ao Tab

--- src/navegacao/Tab.js
+++ src/navegacao/Tab.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
@@ -7,11 +8,30 @@ import TelaC from '../views/TelaC'
 const Tab = createBottomTabNavigator()
 
 export default () => (
-    <Tab.Navigator tabBarOptions={{
-        activeTintColor: 'red',
-        inactiveTintColor: 'blue',
-        labelStyle: { fontSize: 30, fontWeight: 'bold' }
-    }} initialRouteName="TelaB">
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === 'TelaA') {
+                    iconName = focused
+                        ? 'information-circle-outline'
+                        : 'information-outline';
+                } else if (route.name === 'TelaB') {
+                    iconName = focused ? 'pause-circle-outline' : 'pause-outline';
+                } else if (route.name === 'TelaC') {
+                    iconName = focused ? 'pause-circle-outline' : 'pause-outline';
+                }
+
+                return <Ionicons name={iconName} size={size} color={color} />;
+            },
+        })}
+        tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+            // labelStyle: { fontSize: 30, fontWeight: 'bold' },
+        }} initialRouteName="TelaB"
+    >
         <Tab.Screen name='TelaA' component={TelaA} />
         <Tab.Screen name='TelaB' component={TelaB} />
         <Tab.Screen name='TelaC' component={TelaC} />
